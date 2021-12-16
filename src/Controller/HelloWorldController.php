9f889bd5e7766c79c5f69dfb99f63df0821ca026
yelassam <yassine.elassami@altran.com>
2021-12-16 19:16:23 +0100

    Request class in Hello world

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +12,11 @@ class HelloWorldController extends AbstractController
     /**
      * @Route("/hello/world", name="hello_world")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         return $this->render('hello_world/index.html.twig', [
             'controller_name' => 'HelloWorldController',
+            'name' => $name,
         ]);
     }
 }
