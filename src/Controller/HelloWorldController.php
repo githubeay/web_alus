c668cbfc71224cea2f4b10d17e7f4761e5159e70
yelassam <yassine.elassami@altran.com>
2021-12-20 20:13:30 +0100

    Table sql Post, Migration, Entity, Env

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HelloWorldController extends AbstractController
 {
     /**
      * @Route("/hello/world", name="hello_world")
      */
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
+        $post = new Post();
+        $post->setTitle('First Post');
+        $post->setContent('This is our first post');
+
+        $em->persist($post);
+        $em->flush();
+
+        $name = $request->query->get("name");
+        if (!isset($name)){
+            $name = "Alous";
+        }
+        // dd($name);
         return $this->render('hello_world/index.html.twig', [
             'controller_name' => 'HelloWorldController',
             'name' => $name,
